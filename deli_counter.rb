629eafc935c7dd2f20995d61437e751d8dfdd6b8
clifford <clifford@noukod.com>
2018-09-20 18:40:55 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def line
+katz_deli = []
+if  katz_deli.empty?
+  puts "The line is currently empty."
+end
+
+def take_a_number( array,name)
+  array << name
+    position = array.length
+    puts "Welcome, #{name}! You are number #{position} in line."
+    position - 1
+end
