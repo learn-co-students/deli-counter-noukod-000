fc59385a24b2ce11fdd3a51b49a768fc5072d58d
Saint-Jean Germila <germila@noukod.com>
2018-09-26 16:42:29 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,34 @@
-def line(arr)
-  if arr = []
+
+ katz_deli = []
+
+def line(katz_deli)
+  line_positions = [];
+  if katz_deli.empty?
     puts "The line is currently empty."
-end# Write your code here.
+  else
+    number = 1
+    katz_deli.each do |customer|
+      line_positions << "#{number}. #{customer}"
+      number += 1
+    end
+    puts "The line is currently: #{line_positions.join(" ")}"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
+end
+
+   #puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
+   #return other_deli
+# Write your code here.
