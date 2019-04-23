b24ff2001549dd2f96b0c64a111d7c07cd50c622
Alexandro Disla <alexandrodisla@hotmail.com>
2019-04-23 00:15:16 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def line deline
+  if deline.size == 0
+    puts "The line is currently empty."
+  else
+    cline = "The line is currently:"
+    deline.each.with_index(1) do |perso,i|
+      cline << " #{i}. #{perso}"
+    end
+    puts cline
+  end
+end
+
+def take_a_number(deline,nperso)
+  deline << nperso
+  puts "Welcome, #{nperso}. You are number #{deline.count} in line."
+end
+
+def now_serving deline
+  if deline.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deline.first}."
+    deline.shift
+  end
+end
